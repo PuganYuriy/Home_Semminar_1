90ee026856b4e650330b30246e445985d18b4029
YuriyP <solovej-1991@mail.ru>
2022-06-24 22:26:12 +0600

    Первое задание 1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ int number_1 = Convert.ToInt32(Console.ReadLine()); //преобразовыва
 Console.Write("Введите второе целое число: "); // запрашиваем пользователя 2 число
 int number_2 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
 
-int max = 0;
-int min = 0;
-
 if (number_1 >= number_2) // условие сравнить первое число со вторым числом
     Console.WriteLine("Максимальное число = " + number_1 + ", Минимальное число = " + number_2);
 else
