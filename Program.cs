abebdfac1415d1c29735b69f5f08c71f83fa2b8e
YuriyP <solovej-1991@mail.ru>
2022-06-24 22:19:22 +0600

    Первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: "); // запрашиваем пользователя 1 число
+int number_1 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
+
+Console.Write("Введите второе целое число: "); // запрашиваем пользователя 2 число
+int number_2 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
+
+int max = 0;
+int min = 0;
+
+if (number_1 >= number_2) // условие сравнить первое число со вторым числом
+    Console.WriteLine("Максимальное число = " + number_1 + ", Минимальное число = " + number_2);
+else
+    Console.WriteLine("Максимальное число = " + number_2 + ", Минимальное число = " + number_1);
